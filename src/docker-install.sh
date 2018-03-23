8cd2128cde76f7694ea8766a628c9f0ac1804f98
roma_bb8 <jungle.romabb8@gmail.com>
2018-03-23 15:35:55 +0200

    up

--- src/docker-install.sh
+++ src/docker-install.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
-
-apt-cache policy docker-engine
-
-sudo apt-get install docker-engine -y
-
-systemctl status docker.service
-
-echo 'sudo usermod -aG docker $(whoami)'
-echo 'restart linux'
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
